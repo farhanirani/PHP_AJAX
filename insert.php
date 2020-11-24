f08db884551227930b0abc7668cd8a18563a19b6
Farhan Irani <farhanirani007@gmail.com>
2020-11-24 21:10:27 +0530

    database and insert query change

--- insert.php
+++ insert.php
@@ -4,11 +4,11 @@ include("connection.php");
 $data = file_get_contents("php://input");
 $mydata = json_decode($data, true);
 
-$message = $mydata['message'];
-$sender = $mydata['sender'];
+$message = addslashes($mydata['message']);
+$sender = addslashes($mydata['sender']);
 
 if (!empty($message) && !empty($sender)) {
-    $sql = "INSERT INTO messages(`message`, `sender`) VALUES (`$message`,`$sender`)";
+    $sql = "INSERT INTO messages(`message`, `sender`) VALUES ('$message','$sender')";
     $res = mysqli_query($conn, $sql);
     if ($res) {
         echo "Sent";
